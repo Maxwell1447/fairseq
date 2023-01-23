b446cee5f796451f65856b05c47f104c7d59d3a4
Maxime Bouthors <maxime.bouthors@universite-paris-saclay.fr>
2023-01-23 11:51:04 +0100

    clean by removing debug file from index

--- debug_squash.sh
+++
@@ -1,8 +0,0 @@
-INFER_SCRIPTS_PATH=/linkhome/rech/genrqo01/ufn16wp/NLP4NLP/scripts/multi-lev/scripts/infer
-DATA_GEN_PATH=/linkhome/rech/genrqo01/ufn16wp/NLP4NLP/DATA/multi-domain/generated
-
-# cd $INFER_SCRIPTS_PATH
-# source $INFER_SCRIPTS_PATH/run_sbatch_infer_lev.sh
-
-cd $DATA_GEN_PATH
-source $DATA_GEN_PATH/run_eval.sh
